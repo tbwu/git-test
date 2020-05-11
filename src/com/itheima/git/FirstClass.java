12d9c3b9856be2c61062a49f655f5df7de0469f3
admin <291256823@qq.com>
2020-05-11 14:48:16 +0800

    测试第一次提交FirstClass

--- src/com/itheima/git/FirstClass.java
+++ src/com/itheima/git/FirstClass.java
@@ -3,6 +3,12 @@ package com.itheima.git;
 public class FirstClass {
 
     public static void main(String[] args) {
-        System.out.println("第一个测试");
+        System.out.println("?????");
+
+
+    }
+    public static void test() {
+        int i = 0;
+        i += 10;
     }
 }
