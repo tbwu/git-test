11e68b89f2aa4c5adce5f7b10778f8f056e6af76
admin <291256823@qq.com>
2020-05-11 15:13:14 +0800

    本次修改 打印i

--- src/com/itheima/git/FirstClass.java
+++ src/com/itheima/git/FirstClass.java
@@ -5,11 +5,15 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("?????");
 
-
+        //hello test
     }
     public static void test() {
         int i = 0;
         i += 10;
         System.out.println(i);
+
+
+        System.out.println("打印I"+i);
+
     }
 }
